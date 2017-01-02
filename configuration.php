0ed6b58e13496013b40dd0d0c7216415a9e4b961
Wan Zulkarnain <wanzulkarnain69@gmail.com>
2017-01-02 16:28:14 +0800

    Update configuration.php

--- configuration.php
+++ configuration.php
@@ -2,20 +2,21 @@
 /**
  * Instruction: 
  *  
- * 1. Replace the APIKEY with your API Key
- * 2. Replace the COLLECTION with your CollectionID
- * 3. Replace the http://www.google.com/ with your FULL PATH TO YOUR WEBSITE. It must be end with trailing slash "/"
- * 4. Replace the http://www.google.com/success.html with your FULL PATH TO YOUR SUCCESS PAGE. 
- * 5. No need to change $mode (Default: 'Production'). Change to 'Staging' only if you want to test with
+ * 1. Replace the APIKEY with your API Key.
+ * 2. Replace the COLLECTION with your CollectionID.
+ * 3. No need to change $mode (Default: 'Production'). Change to 'Staging' only if you want to test with
  *      https://billplz-staging.herokuapp.com
- * 6. OPTIONAL: Set $fallbackurl if the user are failed to be redirected to the Billplz Payment Page
+ * 4. OPTIONAL: Replace the http://www.google.com/ with your FULL PATH TO YOUR WEBSITE. It must be end with trailing slash "/". *The URL can be overriden later
+ * 5. OPTIONAL: Replace the http://www.google.com/success.html with your FULL PATH TO YOUR SUCCESS PAGE. *The URL can be overriden later
+ * 6. OPTIONAL: Set $amount value.
+ * 6. OPTIONAL: Set $fallbackurl if the user are failed to be redirected to the Billplz Payment Page.
  * 
  */
 $api_key = 'APIKEY';
 $collection_id = 'COLLECTION';
+$mode = 'Production';
+
 $websiteurl = 'http://www.google.com/';
 $successpath = 'http://www.google.com/success.html';
-
-$mode = 'Production';
 $amount = ''; //Example (RM13.50): $amount = '13.50';
 $fallbackurl = ''; //Example: $fallbackurl = 'http://www.google.com/pay.php';
