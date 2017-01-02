8dab255cfe32072136911f05b1c9e0c200f837c2
Wan Zulkarnain <wanzulkarnain69@gmail.com>
2017-01-02 16:59:58 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,14 @@
 <form method="post" action="billplzpost.php">
-    <input type="hidden" name='nama' value='Wan Zulkarnan'>
-    <input type='hidden' name='email' value='emailsini@ymail.com'>
-    <input type='hidden' name="telefonbimbit" value="0121234567">
+    <input type="text" name='nama' required value='Wan Zulkarnan'>
+    <input type="text" name='email' required value='emailsini@ymail.com'>
+    <input type="hidden" name="telefonbimbit" value="0121234567">
     <input type="hidden" name="amaun" value ="54.30">
     <!--Put deliver value as "ya" to enable notification-->
     <input type="hidden" name="deliver" value ="">
     <input type="hidden" name="reference_label" value="">
     <input type="hidden" name="reference_1" value="7">
     <input type="hidden" name="description" value ="apa2">
+    <input type="hidden" name="successpath" value = "">
     <input type="submit">
     
 </form>
