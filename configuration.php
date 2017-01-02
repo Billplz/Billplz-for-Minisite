a5ce34fab9174d3434431c5f33de6e810e4ff505
Wan Zulkarnain <wanzulkarnain69@gmail.com>
2017-01-02 16:37:37 +0800

    Update configuration.php

--- configuration.php
+++ configuration.php
@@ -6,8 +6,8 @@
  * 2. Replace the COLLECTION with your CollectionID.
  * 3. No need to change $mode (Default: 'Production'). Change to 'Staging' only if you want to test with
  *      https://billplz-staging.herokuapp.com
- * 4. OPTIONAL: Replace the http://www.google.com/ with your FULL PATH TO YOUR WEBSITE. It must be end with trailing slash "/". *The URL can be overriden later
- * 5. OPTIONAL: Replace the http://www.google.com/success.html with your FULL PATH TO YOUR SUCCESS PAGE. *The URL can be overriden later
+ * 4. Replace the http://www.google.com/ with your FULL PATH TO YOUR WEBSITE. It must be end with trailing slash "/". *The URL can be overriden later
+ * 5. Replace the http://www.google.com/success.html with your FULL PATH TO YOUR SUCCESS PAGE. *The URL can be overriden later
  * 6. OPTIONAL: Set $amount value.
  * 6. OPTIONAL: Set $fallbackurl if the user are failed to be redirected to the Billplz Payment Page.
  * 
