6f4ce0f8d07256fc2bd623cedb708fe9398a931d
Wan Zulkarnain <wanzulkarnain69@gmail.com>
2017-04-12 12:58:10 +0800

    Update verifytrans.php

--- verifytrans.php
+++ verifytrans.php
@@ -5,30 +5,39 @@ require_once 'configuration.php';
 
 class verifytrans {
 
-    var $billplz;
+    //var $billplz;
     var $data;
+    //var $moreData;
 
     function __construct() {
-        if (isset($_GET['billplz']['id'])) {
-            $this->billplz = new billplz;
-        } else {
-            exit('Fake Request!');
-        }
+        /*
+         * Get Data. Die if input is tempered or X Signature not enabled
+         */
+
+        $this->data = billplz::getRedirectData($x_signature);
+        //$this->billplz = new billplz;
     }
 
     function checkStatus() {
-        global $api_key, $mode;
-        $bill_id = filter_var($_GET['billplz']['id'], FILTER_SANITIZE_STRING);
-        $this->data = $this->billplz->check_bill($api_key, $bill_id, $mode);
+        //global $api_key, $mode;
+        //$bill_id = $this->data['id'];
+        //$this->moreData = $this->billplz->check_bill($api_key, $bill_id, $mode);
         return $this;
     }
+
+    /*
+     * Dalam variable $this->data ada maklumat berikut:
+     * 1. id //bill_id
+     * 2. paid_at
+     * 3. paid
+     * 4. x_signature
+     */
 
     function process() {
         global $successpath;
         if ($this->data['paid']) {
             //////////////////////////////////////////////////
             // Include tracking code here
-            
             //////////////////////////////////////////////////
             if (isset($_GET['successpath'])) {
                 header('Location: ' . base64_decode($_GET['successpath']));
@@ -43,4 +52,5 @@ class verifytrans {
 }
 
 $verifytrans = new verifytrans();
-$verifytrans->checkStatus()->process();
+//$verifytrans->checkStatus()->process();
+$verifytrans->process();
