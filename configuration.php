e74c756f285c34d8cd76ddeec4ef82f3ed14541f
Wan Zulkarnain <wanzulkarnain69@gmail.com>
2017-05-05 15:21:48 +0800

    Update configuration.php

--- configuration.php
+++ configuration.php
@@ -3,20 +3,17 @@
  * Instruction: 
  *  
  * 1. Replace the APIKEY with your API Key.
- * 2. Replace the COLLECTION with your CollectionID.
+ * 2. OPTIONAL: Replace the COLLECTION with your Collection ID.
  * 3. Replace the X_SIGNATURE with your X Signature Key
- * 4. No need to change $mode (Default: 'Production'). Change to 'Staging' only if you want to test with
- *      https://billplz-staging.herokuapp.com
- * 5. Replace the http://www.google.com/ with your FULL PATH TO YOUR WEBSITE. It must be end with trailing slash "/".
- * 6. Replace the http://www.google.com/success.html with your FULL PATH TO YOUR SUCCESS PAGE. *The URL can be overriden later
- * 7. OPTIONAL: Set $amount value.
- * 8. OPTIONAL: Set $fallbackurl if the user are failed to be redirected to the Billplz Payment Page.
+ * 4. Replace the http://www.google.com/ with your FULL PATH TO YOUR WEBSITE. It must be end with trailing slash "/".
+ * 5. Replace the http://www.google.com/success.html with your FULL PATH TO YOUR SUCCESS PAGE. *The URL can be overridden later
+ * 6. OPTIONAL: Set $amount value.
+ * 7. OPTIONAL: Set $fallbackurl if the user are failed to be redirected to the Billplz Payment Page.
  * 
  */
 $api_key = 'APIKEY';
 $collection_id = 'COLLECTION';
 $x_signature = 'X_SIGNATURE';
-$mode = 'Production';
 
 $websiteurl = 'http://www.google.com/';
 $successpath = 'http://www.google.com/success.html';
